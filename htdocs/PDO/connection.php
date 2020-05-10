cf73731be6407bdd1e67f648476e00e2bed4da0c
Olivier Guillemot <leojen23@hotmail.com>
2020-05-10 15:18:59 +0200

    initialisation git

---
+++ htdocs/PDO/connection.php
@@ -0,0 +1,15 @@
+<?php
+try
+{
+    $db = new PDO('mysql:host=127.0.0.1;dbname=Product-hunt;charset=utf8','root','', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ]);
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+
+?>
---
+++ htdocs/index.php
@@ -0,0 +1,41 @@
+<?php
+include './PDO/connection.php';
+
+
+// echo '<pre>' . var_export($data, true) . '</pre>';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product-Hunt</title>
+
+    <!-- Materialize CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- CUSTOM CSS -->
+    <link rel="stylesheet" href="./CSS/style.css">
+</head>
+
+
+<body>
+
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur veniam odit fuga cumque optio error, accusantium, earum voluptates obcaecati modi nulla cupiditate voluptatibus illum debitis iure laborum. Architecto quibusdam exercitationem nulla similique ab, eos eligendi quasi consequatur commodi deleniti itaque repellendus corporis sed repellat cupiditate illum eum facilis quaerat eaque officia dolor odit esse sunt. Maxime veritatis sint et molestias quisquam? Aliquam quis dolore ipsam incidunt quam repellat corporis minima eaque modi deserunt? Voluptates distinctio accusamus sequi harum iusto fuga culpa porro dolor iste amet ea repellendus quasi tenetur quia assumenda nobis, animi vero quibusdam consequuntur? Est ex accusantium dolorem.</p>
+
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, perspiciatis.</p>
+    <div id="hello">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam facilis odio assumenda eius! Velit alias ea perferendis, earum iste saepe veniam mollitia dignissimos veritatis commodi!</p>
+    </div>
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="../JS/script.js"></script>
+
+</body>
+
+</html>
